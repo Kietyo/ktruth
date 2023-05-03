04dcea203451705644b345e56ba21416b835eaea
Kietyo <kietmsm@gmail.com>
2023-05-02 20:41:46 -0700

    Moving some files, part 6.

--- src/commonMain/kotlin/com/kietyo/ktruth/AnySubject.kt
+++ src/commonMain/kotlin/com/kietyo/ktruth/AnySubject.kt
@@ -18,6 +18,6 @@ class AnySubject<T : Any>(val actual: T) {
     inline fun <reified E : Any> isInstanceOf(value: Any?): E {
         contract { returns() implies (value is E) }
         assertIs<E>(actual)
-        return actual as E
+        return actual
     }
 }
