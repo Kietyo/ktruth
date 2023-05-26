0df5df5d69b4ddecfcfc00c9255ba622ee1a73fb
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-05-26 00:53:38 -0700

    Trying to get ktruth working with kproject.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,15 @@
 pluginManagement {
+    repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }
     val kotlinVersion: String by settings
     plugins {
         kotlin("multiplatform") version kotlinVersion
     }
 }
+
+plugins {
+    id("com.soywiz.kproject.settings") version "0.3.1"
+}
+
 
 rootProject.name = "ktruth"
 
